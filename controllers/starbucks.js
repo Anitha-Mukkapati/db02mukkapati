d54702bdc7e78b239d85a4af1b677fcf0d306b0e
Anitha-Mukkapati <anithamukkapati498@gmail.com>
2021-11-12 12:05:15 -0600

    first commit

--- controllers/starbucks.js
+++ controllers/starbucks.js
@@ -13,9 +13,16 @@ exports.starbucks_list = async function (req, res) {
 };
 
 // for a specific starbucks. 
-exports.starbucks_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: starbucks detail: ' + req.params.id);
-};
+exports.starbucks_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await starbucks.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 
 // Handle starbucks create on POST. 
 exports.starbucks_create_post =  async function(req, res) {
@@ -60,3 +67,23 @@ exports.starbucks_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+exports.costume_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Costume.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.costume_type)  
+               toUpdate.costume_type = req.body.costume_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
